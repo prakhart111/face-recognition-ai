882bff1ca5479ab917d62db4848c196e567d5803
Prakhar <ptofficial29@gmail.com>
2024-04-24 19:24:05 +0530

    main py done, to be tested;

--- backend/main.py
+++ backend/main.py
@@ -12,22 +12,41 @@ def load_final_model():
     print("Model loaded")
     return model
 
+def img_preprocess(img):
+    # resize to 128x128
+    img = img.resize((128, 128))
+    return img
 
-def predict(img: Image.Image):
-    img = img.resize((64, 64))
-    img = image.img_to_array(img)
-    img = img / 255
-    img = np.expand_dims(img, axis = 0)
+def predictSimilarImage(anchor: Image.Image, img1: Image.Image, img2: Image.Image):
+    anchor = img_preprocess(anchor)
+    img1 = img_preprocess(img1)
+    img2 = img_preprocess(img2)
 
     classifier = load_final_model()
 
-    prediction = classifier.predict(img, batch_size = None, steps = 1)[0] * 100
-    prediction = prediction.tolist()
-    return prediction
+    # jiska zero aaya that's +ve 
+    anchorWithImg1Test = classifier.classify_images(anchor, img1) # 0 
+    # jiska one aaya that's -ve
+    anchorWithImg2Test = classifier.classify_images(anchor, img2) # 1
 
+
+    if anchorWithImg1Test <= anchorWithImg2Test:
+        # return "Image 1 is more similar to Anchor Image"
+        return True
+    else:
+        # return "Image 2 is more similar to Anchor Image"
+        return False
+ 
+ # for api
 def read_imagefile(file) -> Image.Image:
     img = Image.open(BytesIO(file)) #Pillow object
     return img
+
+def file_check(file):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return False
+    return True
 
 
 app = FastAPI()
@@ -36,26 +55,32 @@ app = FastAPI()
 async def testing():
     return "Hello World"
 
-@app.post("/predict/image")
-async def predict_api(file: UploadFile = File(...)):
+@app.post("/predict")
+async def predict_api(fileAnchor: UploadFile = File(...), file1: UploadFile = File(...), file2: UploadFile = File(...)):
 
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
-    if not extension:
+    # extension check
+    if not file_check(fileAnchor):
+        return {"Error" : "Image must be jpg or png format!"}
+    if not file_check(file1):
+        return {"Error" : "Image must be jpg or png format!"}
+    if not file_check(file2):
         return {"Error" : "Image must be jpg or png format!"}
         
-    image = read_imagefile(await file.read())
+    anchor = read_imagefile(await fileAnchor.read())
+    img1 = read_imagefile(await file1.read())
+    img2 = read_imagefile(await file2.read())
 
-    prediction = predict(image)
+    try:
+        prediction = predictSimilarImage(anchor, img1, img2)
+    except Exception as e:
+        return {"Error": str(e)}
 
     final_prediction = dict()
 
-    final_prediction.update({"Dog Percentage" : prediction[0]})
-    final_prediction.update({"Cat Percentage" : 100 - prediction[0]})
-    if prediction[0] >= 50:
-        final_prediction.update({"Label" : "Dog"})
-    
-    elif prediction[0] < 50:
-        final_prediction.update({"Label" : "Cat"})
+    if prediction:
+        final_prediction.update({"Result" : "Image 1 is more similar to Anchor Image"})
+    else:
+        final_prediction.update({"Label" : "Image 2 is more similar to Anchor Image"})
 
     return final_prediction
 
